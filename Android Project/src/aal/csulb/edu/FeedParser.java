ac66e9023b3648c4cf2ac5b01b64008750a52502
Alex Chavez <alex@alexchavez.net>
2011-03-03 23:39:29 -0800

    Updated project files.

--- Android Project/src/aal/csulb/edu/FeedParser.java
+++
@@ -1,6 +0,0 @@
-package aal.csulb.edu;
-import java.util.List;
-
-public interface FeedParser {
-	List<Message> parse();
-}
--- Android Project/src/aal/csulb/edu/ParserType.java
+++
@@ -1,7 +0,0 @@
-package aal.csulb.edu;
-
-public enum ParserType {
-	
-	SAX,DOM,ANDROID_SAX,XML_PULL;
-
-}
