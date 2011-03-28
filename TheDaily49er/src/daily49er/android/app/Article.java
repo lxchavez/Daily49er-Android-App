1b43a38a3a23cd21e153d6861d9000213104f778
lxchavez <alex@alexchavez.net>
2011-03-27 19:30:36 -0700

    Changed local git directory.

---
+++ TheDaily49er/src/daily49er/android/app/Article.java
@@ -0,0 +1,51 @@
+package daily49er.android.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import daily49er.android.app.NewsTab;
+public class Article extends ListActivity {
+	//private List<Message> message;
+	private List<Message> messageList;
+	private Long num;
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.list);
+		num = this.getIntent().getExtras().getLong("position");
+		loadFeed();
+	}
+	private void loadFeed(){
+		try{
+			String temp;
+			FeedParser parser = FeedParserFactory.getParser();
+			messageList = parser.parse();
+			List<String> titles = new ArrayList<String>(messageList.size());
+			//for (Message msg : message){
+			String castLong = Long.toString(num);
+			int index = Integer.parseInt(castLong);
+			temp = messageList.get(index).getDescription();
+			int dot = temp.indexOf("...");
+			temp = temp.replaceAll("&nbsp;", "");
+			int disclaimer = temp.indexOf("Disclaimer:");
+			temp = temp.substring(dot+3, disclaimer).trim();
+			temp = "\t" + temp;
+			titles.add(temp);
+			//titles.add(n.messageList.get(n.messageList.).getDescription());
+			//titles.add(Integer.toString(n.messageList.get(0).getOrder() - n.messageList.size()));
+			//}
+			ArrayAdapter<String> adapter = 
+				new ArrayAdapter<String>(this, R.layout.row,titles);
+			this.setListAdapter(adapter);
+
+		}catch(Throwable t){
+			Log.e("AndroidNews",t.getMessage(),t);
+		}
+	}
+
+}
---
+++ TheDaily49er/src/daily49er/android/app/NewsTab.java
@@ -0,0 +1,81 @@
+package daily49er.android.app;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import org.xmlpull.v1.XmlSerializer;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Xml;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class NewsTab extends ListActivity 
+{
+	private static List<Message> messageList;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+	
+		//setContentView(android.R.layout.simple_list_item_1);
+		//setListAdapter(new ArrayAdapter<String>(this,
+				//android.R.layout.simple_list_item_1, VIDEOS));
+		
+		getListView().setTextFilterEnabled(true);
+		loadFeed();
+	}
+	
+	public void loadFeed(){
+		try{
+		String temp;
+		FeedParser parser = FeedParserFactory.getParser();
+		messageList = parser.parse();
+		List<String> titles = new ArrayList<String>(messageList.size());
+    	for (Message msg : messageList){
+    		//titles.add(msg.getDescription());
+    		temp = msg.getTitle();
+    		temp = temp + " \nby " + msg.getAuthor();  
+    		titles.add(temp);
+    		 //titles.add(Integer.toString(msg.getOrder()));
+    		//titles.add(msg.getDate());
+    		//titles.add(msg.getAuthor());
+    		//titles.add(msg.getP());
+    	}
+   
+    	ArrayAdapter<String> adapter = 
+    		new ArrayAdapter<String>(this, R.layout.row,titles);
+    	this.setListAdapter(adapter);
+		}
+		catch (Throwable t){
+    		Log.e("AndroidNews",t.getMessage(),t);
+    	}
+
+	}
+
+	//This is what to do when an item from the list is clicked
+	protected void onListItemClick(ListView l, View v, int position, long id) 
+	{
+		super.onListItemClick(l, v, position, id);
+		Intent viewMessage = new Intent(this,Article.class);
+		viewMessage.putExtra("position", id);
+		this.startActivity(viewMessage);
+
+		//Intent viewMessage = new Intent(Intent.ACTION_VIEW, 
+		//Uri.parse(message.get(position).getLink().toExternalForm()));
+		//this.startActivity(viewMessage);
+		//Object o = this.getListAdapter().getItem(position);
+		//String name = o.toString();
+		//Toast.makeText(this, "Beachin' Software " + " " + name, Toast.LENGTH_LONG).show();
+	}
+	
+
+}
