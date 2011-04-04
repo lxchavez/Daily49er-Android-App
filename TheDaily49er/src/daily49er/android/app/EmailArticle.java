8fb0daba1b5631a9f05ef950b0063021beddd6a7
Alex Chavez <alex@alexchavez.net>
2011-04-03 23:57:48 -0700

    Added a test implementation of opening a launcher for an e-mail app and passing in a test subject and body.

--- TheDaily49er/src/daily49er/android/app/EmailArticle.java
+++ TheDaily49er/src/daily49er/android/app/EmailArticle.java
@@ -1,6 +1,25 @@
 package daily49er.android.app;
 
-public class EmailArticle 
-{
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
+/**
+ * This  is the implementation  for will handle sharing an article via 
+ * e-mail by creating a new e-mail intent and launching the default 
+ * e-mail application as set by the user. 
+ * @author Alex Chavez
+ */
+public class EmailArticle extends Activity 
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND); 
+		emailIntent.setType("plain/text");
+		emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Test subject");
+		emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "The message body."); 
+		startActivity(Intent.createChooser(emailIntent, "Share your article through:"));
+	}
+	
 }
