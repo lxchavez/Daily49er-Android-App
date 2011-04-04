40990c0c74484835fdb87cac6a343443c3af775d
lxchavez <alex@alexchavez.net>
2011-04-04 14:08:18 -0700

    Added a new URL: http://alexchavez.net/daily-49er-rss-1.111.111 This will be used for demonstration purposes.

--- TheDaily49er/src/daily49er/android/app/FeedParserFactory.java
+++ TheDaily49er/src/daily49er/android/app/FeedParserFactory.java
@@ -2,7 +2,8 @@ package daily49er.android.app;
 
 public class FeedParserFactory {
 	//static String feedUrl =  "http://rss.news.yahoo.com/rss/un";
-	static String feedUrl =  "http://www.daily49er.com/se/daily-49er-rss-1.111?detailRSS=true";
+	static String feedUrl = "http://alexchavez.net/daily-49er-rss-1.111.111";
+	//static String feedUrl =  "http://www.daily49er.com/se/daily-49er-rss-1.111?detailRSS=true";
 	
 	public static FeedParser getParser(){
 		return new AndroidSaxFeedParser(feedUrl);
