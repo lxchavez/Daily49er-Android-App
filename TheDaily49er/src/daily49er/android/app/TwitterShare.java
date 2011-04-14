d4a7912d60808ae77fa605b8402fabf6c31c846d
Alex Chavez <alex@alexchavez.net>
2011-04-14 09:16:58 -0700

    Started Twitter integration.

--- TheDaily49er/src/daily49er/android/app/TwitterShare.java
+++ TheDaily49er/src/daily49er/android/app/TwitterShare.java
@@ -4,6 +4,7 @@ import com.facebook.android.Facebook;
 
 import android.app.Activity;
 import android.os.Bundle;
+
 import winterwell.jtwitter.*;
 import oauth.signpost.*;
 
@@ -12,7 +13,7 @@ public class TwitterShare extends Activity
 	private final String JTWITTER_OAUTH_KEY = "";
 	private final String JTWITTER_OAUTH_SECRET = "";
 	
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. **/
     @Override
     public void onCreate(Bundle savedInstanceState) 
     {
@@ -24,7 +25,6 @@ public class TwitterShare extends Activity
     			JTWITTER_OAUTH_SECRET, "oob");
     	
     	oauthClient.authorizeDesktop();	
-    }
-	
+    } //end onCreate()
 
 }
