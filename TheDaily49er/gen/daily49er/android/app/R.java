0054677181d0635c035c1587261dbd99b234d8db
Alex Chavez <alex@alexchavez.net>
2011-04-15 01:00:16 -0700

    Added accidentally deleted article_page.xml file.

--- TheDaily49er/gen/daily49er/android/app/R.java
+++ TheDaily49er/gen/daily49er/android/app/R.java
@@ -18,33 +18,39 @@ public final class R {
         public static final int yellow=0x7f070000;
     }
     public static final class drawable {
-        public static final int globe=0x7f020000;
-        public static final int ic_tab_events=0x7f020001;
-        public static final int ic_tab_home=0x7f020002;
-        public static final int ic_tab_media=0x7f020003;
-        public static final int ic_tab_settings=0x7f020004;
-        public static final int icon=0x7f020005;
-        public static final int movie=0x7f020006;
-        public static final int settings=0x7f020007;
-        public static final int splash_screen=0x7f020008;
-        public static final int tiles=0x7f020009;
+        public static final int facebook_icon=0x7f020000;
+        public static final int globe=0x7f020001;
+        public static final int ic_tab_events=0x7f020002;
+        public static final int ic_tab_home=0x7f020003;
+        public static final int ic_tab_media=0x7f020004;
+        public static final int ic_tab_settings=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int movie=0x7f020007;
+        public static final int settings=0x7f020008;
+        public static final int splash_screen=0x7f020009;
+        public static final int tiles=0x7f02000a;
     }
     public static final class id {
-        public static final int ImageView01=0x7f080006;
-        public static final int TextView01=0x7f080005;
-        public static final int email_share=0x7f080004;
-        public static final int facebook_share=0x7f080002;
-        public static final int podcastButton=0x7f080001;
-        public static final int twitter_share=0x7f080003;
-        public static final int videoButton=0x7f080000;
+        public static final int ImageView01=0x7f08000a;
+        public static final int TextView01=0x7f080009;
+        public static final int articleview=0x7f080003;
+        public static final int authorview=0x7f080001;
+        public static final int categoryview=0x7f080002;
+        public static final int email_share=0x7f080008;
+        public static final int facebook_share=0x7f080006;
+        public static final int podcastButton=0x7f080005;
+        public static final int titleview=0x7f080000;
+        public static final int twitter_share=0x7f080007;
+        public static final int videoButton=0x7f080004;
     }
     public static final class layout {
-        public static final int list=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int media=0x7f030002;
-        public static final int menu_button=0x7f030003;
-        public static final int row=0x7f030004;
-        public static final int splash=0x7f030005;
+        public static final int article_page=0x7f030000;
+        public static final int list=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int media=0x7f030003;
+        public static final int menu_button=0x7f030004;
+        public static final int row=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f060001;
